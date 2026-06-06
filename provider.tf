7f966f498b53a364694ff670dfa6cfeed0133037
Naveen Kumar <marapareddynaveen678@gmail.com>
2026-06-06 14:37:26 +0530

    actions

---
+++ provider.tf
@@ -0,0 +1,20 @@
+  terraform {
+    required_providers {
+      aws = {
+        source  = "hashicorp/aws"
+        version = "6.36.0"
+      }
+    }
+
+    backend "s3" {
+      bucket   = "remote-state-aws-dev-bucket-245758552785-us-east-1-an"     # Bucket name
+      key      = "roboshop-dev-vpc"      
+      region   = "us-east-1"
+      encrypt  = true
+      use_lockfile = true
+    }
+  }
+
+  provider "aws" {
+    region = "us-east-1"
+  }
